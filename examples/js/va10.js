4113c6dc2e29229d2fe81b9b5c4c52bf94924d68
Aaron Gussman <agussman@thehumangeo.com>
2017-03-05 19:20:11 -0500

    Some basic pass at an example for VA10

--- examples/js/va10.js
+++ examples/js/va10.js
@@ -35,6 +35,43 @@ $(document).ready(function() {
 	var fillColorFunctionRomney = new L.HSLLuminosityFunction(new L.Point(0, 0.95), new L.Point(55, 0.4), {outputHue: 0, outputSaturation: '100%'});
 	var fillColorFunctionObama = new L.HSLLuminosityFunction(new L.Point(0, 0.95), new L.Point(55, 0.4), {outputHue: 240, outputSaturation: '100%'});
 
-	var opacityFunction = new L.PiecewiseFunction([new L.LinearFunction(new L.Point(0, 0), new L.Point(1, 0.7)), new L.LinearFunction(new L.Point(1, 0.7), new L.Point(55, 0.7))]);
+	var opacityFunction = new L.PiecewiseFunction([new L.LinearFunction(new L.Point(0, 0), new L.Point(1, 0.7)), new L.LinearFunction(new L.Point(1, 0.7), new L.Point(1000, 0.7))]);
+
+	var options = {
+		recordsField: null,
+		geoJSONField: 'geojson',
+		locationMode: L.LocationModes.GEOJSON,
+		codeField: null,
+		locationTextField: 'geojson.properties.name',
+		displayOptions: {
+			'data': {
+				displayName: 'Data Value',
+				fillColor: fillColorFunctionObama,
+				fillOpacity: opacityFunction
+			}
+		},
+		includeLayer: function (record) {
+			console.log(record);
+			return true;
+		},
+		layerOptions: {
+			fillOpacity: 0.5,
+			opacity: 1,
+			weight: 1,
+			stroke: false,
+			color: '#0000FF'
+		},
+		tooltipOptions: {
+			iconSize: new L.Point(80,60),
+			iconAnchor: new L.Point(-5,60)
+		}
+	};
+
+	console.log("This is for real");
+
+	var electionLayer = new L.ChoroplethDataLayer(va10geoJSON,options);
+
+	map.addLayer(electionLayer);
+
 
 });
